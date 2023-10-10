983bdefd6c22cfce5059fa640c511ccd29546b05
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-10 18:12:59 +0200

    Added client manager

---
+++ Assets/Scripts/Networking/Client/ClientSingleton.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ClientSingleton : MonoBehaviour
+{
+    private ClientGameManager gameManager;
+    private static ClientSingleton instance;
+    public static ClientSingleton Instance
+    {
+        get
+        {
+            if (instance != null) return instance;
+            instance = FindObjectOfType<ClientSingleton>();
+            if (instance == null)
+            {
+                Debug.LogError("No ClientSingleton in the scene");
+                return null;
+            }
+
+            return instance;
+        }
+    }
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public async Task CreateClient()
+    {
+       gameManager = new ClientGameManager();
+       await gameManager.InitAsync();
+    }
+}
