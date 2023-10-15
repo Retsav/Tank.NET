7a5eb04a7160a45b9173fafc5e240ee8b2350f2e
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-15 20:43:00 +0200

    Authentication Wrapper improvements

--- Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
+using Unity.Services.Core;
 using UnityEngine;
 
 public static class AuthenticationWrapper
@@ -14,25 +13,62 @@ public static class AuthenticationWrapper
             return AuthState;
         }
 
-        AuthState = AuthState.Authenticating;
-        int tries = 0;
-        while (AuthState == AuthState.Authenticating && tries < maxTries)
+        if (AuthState == AuthState.Authenticating)
         {
-            await AuthenticationService.Instance.SignInAnonymouslyAsync();
-            if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
-            {
-                AuthState = AuthState.Authenticated;
-                break;
-            }
-            tries++;
-            await Task.Delay(1000);
-            
+            Debug.LogWarning("Already authenticating...");
+            await Authenticating();
+            return AuthState;
+        }
+        await SignInAnonymouslyAsync(maxTries);
+        return AuthState;
+    }
+
+    private static async Task<AuthState> Authenticating()
+    {
+        while (AuthState == AuthState.Authenticating || AuthState == AuthState.NotAuthenticated)
+        {
+            await Task.Delay(200);
         }
 
         return AuthState;
     }
-}
 
+    private static async Task SignInAnonymouslyAsync(int maxTries = 5)
+    {
+        AuthState = AuthState.Authenticating;
+        int tries = 0;
+        while (AuthState == AuthState.Authenticating && tries < maxTries)
+        {
+            try
+            {
+                await AuthenticationService.Instance.SignInAnonymouslyAsync();
+                if (AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+                {
+                    AuthState = AuthState.Authenticated;
+                    break;
+                }
+            }
+            catch (AuthenticationException ex)
+            {
+                Debug.LogError(ex);
+                AuthState = AuthState.Error;
+            }
+            catch (RequestFailedException exception)
+            {
+                Debug.LogError(exception);
+                AuthState = AuthState.Error;
+            }
+            tries++;
+            await Task.Delay(1000);
+        }
+
+        if (AuthState != AuthState.Authenticated)
+        {
+            Debug.LogWarning("Player TimeOut! Tried: " + tries + " times.");
+            AuthState = AuthState.TimeOut;
+        }
+    }
+}
 
 public enum AuthState
 {
