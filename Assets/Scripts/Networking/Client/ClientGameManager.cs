453fd968f14184d9b18a0433093d11d9b0546b89
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-16 12:45:42 +0200

    Added client joining

--- Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -1,13 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using Unity.Networking.Transport.Relay;
 using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ClientGameManager
 {
     private const string MenuSceneName = "Menu";
+    private JoinAllocation allocation;
     public async Task<bool> InitAsync()
     {
         await UnityServices.InitializeAsync();
@@ -23,5 +28,22 @@ public class ClientGameManager
     public void GoToMenu()
     {
         SceneManager.LoadScene(MenuSceneName);
+    }
+
+    public async Task StartClientAsync(string joinCode)
+    {
+        try
+        {
+            allocation = await Relay.Instance.JoinAllocationAsync(joinCode);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e);
+            return;
+        }
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        RelayServerData relayServerData = new RelayServerData(allocation, "dtls");
+        transport.SetRelayServerData(relayServerData);
+        NetworkManager.Singleton.StartClient();
     }
 }
