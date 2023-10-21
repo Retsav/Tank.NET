298472d699721ad4b657ef6a8a3de39741a19c11
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-21 21:45:11 +0200

    Added HealingZone logic to detect when player is in it

---
+++ Assets/Scripts/Core/Combat/HealingZone.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealingZone : NetworkBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Image healPowerBar;
+
+    [Header("Settings")] 
+    [SerializeField] private int maxHealPower = 30;
+    [SerializeField] private float healCooldown = 60f;
+    [SerializeField] private float healTickRate = 1f;
+    [SerializeField] private int coinsPerTick = 10;
+    [SerializeField] private int healthPerTick = 10;
+
+    private List<TankPlayer> playersInZone;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!IsServer) return;
+        if (other.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer tankPlayer))
+        {
+            playersInZone.Add(tankPlayer);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!IsServer) return;
+        if (other.attachedRigidbody.TryGetComponent<TankPlayer>(out TankPlayer tankPlayer))
+        {
+            playersInZone.Remove(tankPlayer);
+        }
+    }
+}
