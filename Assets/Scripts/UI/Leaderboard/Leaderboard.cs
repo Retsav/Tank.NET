20a301ba522ddc6a8a87d1898ebd0c90de583afe
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-21 15:39:41 +0200

    New players spawning in leaderboard WIP

--- Assets/Scripts/UI/Leaderboard/Leaderboard.cs
+++ Assets/Scripts/UI/Leaderboard/Leaderboard.cs
@@ -16,4 +16,73 @@ public class Leaderboard : NetworkBehaviour
     {
         leaderboardEntities = new NetworkList<LeaderboardEntityState>();
     }
+
+    private void HandlePlayerSpawned(TankPlayer player)
+    {
+        leaderboardEntities.Add(new LeaderboardEntityState
+            {
+                ClientId = player.OwnerClientId,
+                PlayerName = player.PlayerName.Value,
+                Coins = 0
+            });
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged += HandleLeaderboardEntitiesChanged;
+            foreach (LeaderboardEntityState entity in leaderboardEntities)
+            {
+                HandleLeaderboardEntitiesChanged(new NetworkListEvent<LeaderboardEntityState>
+                {
+                    Type = NetworkListEvent<LeaderboardEntityState>.EventType.Add,
+                    Value = entity
+                });
+            }
+        }
+        if (!IsServer) return;
+        TankPlayer[] players = FindObjectsByType<TankPlayer>(FindObjectsSortMode.None);
+        foreach (TankPlayer player in players)
+        {
+            HandlePlayerSpawned(player);
+        }
+        TankPlayer.OnPlayerSpawned += HandlePlayerSpawned;
+        TankPlayer.OnPlayerDespawned += HandlePlayerDespawned;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            leaderboardEntities.OnListChanged -= HandleLeaderboardEntitiesChanged;
+        }
+        if (!IsServer) return;
+        TankPlayer.OnPlayerSpawned -= HandlePlayerSpawned;
+        TankPlayer.OnPlayerDespawned -= HandlePlayerDespawned;
+    }
+
+    private void HandleLeaderboardEntitiesChanged(NetworkListEvent<LeaderboardEntityState> changeevent)
+    {
+        switch (changeevent.Type)
+        {
+            case NetworkListEvent<LeaderboardEntityState>.EventType.Add:
+                Instantiate(leaderboardEntityPrefab, leaderboardEntityHolder);
+                break;
+            
+        }
+    }
+
+
+    private void HandlePlayerDespawned(TankPlayer player)
+    {
+        if (leaderboardEntities == null) return;
+        foreach (LeaderboardEntityState entity in leaderboardEntities)
+        {
+            if (entity.ClientId != player.OwnerClientId)
+                continue;
+            leaderboardEntities.Remove(entity);
+            break;
+        }
+    }
 }
