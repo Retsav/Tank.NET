ba7d59d2ec082f5de3fd84c41828a69bd9a81d15
Dominik Dadański <ddadanski@edu.cdv.pl>
2023-10-23 13:27:58 +0200

    Added references to script

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,9 +1,24 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("References")]
     [SerializeField] private TMP_InputField joinCodeField;
+    [SerializeField] private TMP_Text queueTimeText;
+    [SerializeField] private TMP_Text queueStatusText;
+    [SerializeField] private TMP_Text findMatchButtonText;
+
+
+    private void Start()
+    {
+        if (ClientSingleton.Instance == null) return;
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        queueStatusText.text = string.Empty;
+        queueTimeText.text = string.Empty;
+    }
+
     public async void StartHost()
     {
         await HostSingleton.Instance.GameManager.StartHostAsync();
